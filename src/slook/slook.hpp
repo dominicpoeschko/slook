0d67de70910abd5fbe85eeda77d35debc0191894
dominic <mail@dominicp.de>
2023-03-28 14:02:58 +0200

    .

--- src/slook/slook.hpp
+++ src/slook/slook.hpp
@@ -1,21 +1,31 @@
 #include "commands.hpp"
 
-#include <aglio/packager.hpp>
-#include <cstddef>
-#include <span>
-#include <fmt/format.h>
 #include <aglio/format.hpp>
+#include <aglio/packager.hpp>
+#include <chrono>
+#include <cstddef>
+#include <fmt/format.h>
+#include <span>
 
 namespace slook {
-template<template<typename> typename Vector, typename String>
+template<template<typename> typename Vector, typename String, typename SendFunction, typename TimerFunction>
 struct Lookup {
+    SendFunction       send_f;
+    TimerFunction timer_register_f;
+
+    template<typename SendCb, typename TimerRegisterCb>
+    Lookup(SendCb&& send_f_, TimerRegisterCb&& timer_register_f_)
+      : send_f{std::forward<SendCb>(send_f_)}
+      , timer_register_f{std::forward<TimerRegisterCb>(timer_register_f_)} {}
+
     using packager = aglio::Packager<aglio::IPConfig>;
 
-    auto message_callback(std::span<std::byte const> data) {
-
-        auto const p = packager::unpack<slook::CommandSet<std::vector, std::string>>(data);
-        fmt::print("{} {}\n",data.size(), p);
+    void message_callback(std::span<std::byte const> data) {
+        auto const p = packager::unpack<slook::CommandSet<Vector, String>>(data);
+        fmt::print("{} {}\n", data.size(), p);
     }
+
+    void timer_callback() {}
 };
 }   // namespace slook
 
--- src/test.cpp
+++ src/test.cpp
@@ -2,21 +2,30 @@
 #include <fmt/ranges.h>
 //
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
 #include <iostream>
 #include <slook/slook.hpp>
 #include <string_view>
 using boost::asio::ip::udp;
 #include <aglio/format.hpp>
 #include <aglio/packager.hpp>
+#include <functional>
 
 class server {
-    slook::Lookup<std::vector, std::string> lookup;
+    using Clock = std::chrono::steady_clock;
 
 public:
+    void send(std::vector<std::byte> const& data) {}
+
+    void startTimer(std::chrono::milliseconds time) {}
+
     server(boost::asio::io_service& io_service, short port)
-      : io_service_(io_service)
-      , socket_(io_service) {
+      : io_service_{io_service}
+      , lookup{
+          [this](std::vector<std::byte> const& data) { send(data); },
+          [this](std::chrono::milliseconds time) { startTimer(time); },
+        }
+      , socket_{io_service}
+      , timer{io_service} {
         static constexpr auto MulticastAddress = std::string_view{"239.255.13.37"};
 
         auto const ep = udp::endpoint(udp::v4(), port);
@@ -41,24 +50,30 @@ public:
         socket_.async_receive_from(
           boost::asio::buffer(data_, 1024),
           sender_endpoint_,
-          boost::bind(
-            &server::handle_receive_from,
-            this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+          [this](auto ec, auto s) { handle_receive_from(ec, s); });
     }
 
 private:
     boost::asio::io_service& io_service_;
-    udp::socket              socket_;
-    udp::endpoint            sender_endpoint_;
-    std::vector<std::byte>   data_;
+    slook::Lookup<
+      std::vector,
+      std::string,
+      std::function<void(std::vector<std::byte> const&)>,
+      std::function<void(std::chrono::milliseconds)>>
+                                             lookup;
+    udp::socket                              socket_;
+    udp::endpoint                            sender_endpoint_;
+    std::vector<std::byte>                   data_;
+    boost::asio::basic_waitable_timer<Clock> timer;
 };
 
 int main() {
     static constexpr std::uint16_t UDPPort = 7331;
     using namespace boost::asio;
     io_context ioc;
+
+    boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait([&ioc](boost::system::error_code const&, int) { ioc.stop(); });
 
     server s{ioc, UDPPort};
 
